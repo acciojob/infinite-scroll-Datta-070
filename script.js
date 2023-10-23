de7e71d75525f2319253802189b0f79e873ecdd1
Chinmay Datt D <chinmaydatt003@gmail.com>
2023-10-23 17:29:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 //your code here!
+const infilist=document.getElementById('infi-list');
+for(let i=1;i<=10;i++){
+	const li=document.createElement('li');
+	li.textContent=`List item ${i}`;
+	infilist.appendChild(li);
+}
 
+windows.addEventListener('scroll', ()=>{
+	const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+	if(scrollTop + clientHeight >= scrollHeight-7){
+		for(let i=1;i<=2;i++){
+			const li=document.createElement('li');
+			li.textContent=`List item ${i+infilist.children.length}`;
+			infilist.appendChild(li);
+		}
+	}
+	});
